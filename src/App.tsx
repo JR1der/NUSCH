7803b093431381d60b45fa7b264ae1b3bf19b181
IvanPopovych <byd0663@gmail.com>
2024-11-21 21:20:54 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Footer } from "./layout/Footer/Footer";
+import { Header } from "./layout/Header/Header";
+import { HomePage } from "./pages/HomePage/HomePage";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
